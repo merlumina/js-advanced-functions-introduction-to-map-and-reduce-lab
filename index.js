b0082ed8e79622f9d02fec5c3029087a0283d965
Liz Wilson <lizriderwilson@gmail.com>
2021-07-06 11:59:47 -0400

    commit tree a18992cfe0b07d2ef5dfd4b3374bb7b253bbc010

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
-// Your code here
+function mapToNegativize(sourceArray) {
+  let newArray = [];
+  for (let i = 0; i < sourceArray.length; i++) {
+    sourceArray[i] *= -1;
+    newArray.push(sourceArray[i]);
+  }
+  return newArray;
+}
+
+function mapToNoChange(sourceArray) {
+  let newArray = [];
+  for (let i = 0; i < sourceArray.length; i++) {
+    newArray.push(sourceArray[i]);
+  }
+  return newArray;
+}
+
+function mapToDouble(sourceArray) {
+  let newArray = [];
+  for (let i = 0; i < sourceArray.length; i++) {
+    sourceArray[i] *= 2;
+    newArray.push(sourceArray[i]);
+  }
+  return newArray;
+}
+
+function mapToSquare(sourceArray) {
+  let newArray = [];
+  for (let i = 0; i < sourceArray.length; i++) {
+    sourceArray[i] **= 2;
+    newArray.push(sourceArray[i]);
+  }
+  return newArray;
+}
+
+function reduceToTotal(sourceArray, runningTotal = 0) {
+  for (let i = 0; i < sourceArray.length; i++) {
+    runningTotal += sourceArray[i]; 
+  }
+  return runningTotal;
+}
+
+function reduceToAllTrue(sourceArray, truthiness = true) {
+  for (let i = 0; i < sourceArray.length; i++) {
+    if (!sourceArray[i]) {
+      truthiness = false;
+    }
+  }
+    return truthiness;
+}
+
+function reduceToAnyTrue(sourceArray, truthiness = false) {
+  for (let i = 0; i < sourceArray.length; i++) {
+    if (sourceArray[i]) {
+      truthiness = true;
+    }
+  }
+    return truthiness;
+}
